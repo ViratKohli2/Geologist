f9c2d81033e9465624d5eeea2082aad789014063
ViratKohli2 <77227001+ViratKohli2@users.noreply.github.com>
2021-05-01 17:16:39 +0700

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,27 @@
+class Stone {
+    constructor(x, y) {
+      var options = {
+        'density':4,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, 200, 70, options);
+      this.width = 200;
+      this.height = 70;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('white')
+      fill('red')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
